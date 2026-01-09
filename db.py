b738a97bc910e00bb6466fa9a8a6050eb44d1319
Vaibhav15-D <vaibhavdukare02@gmail.com>
2026-01-09 13:22:51 +0530

    Update db.py

--- db.py
+++ db.py
@@ -5,11 +5,3 @@ import psycopg2
 def get_connection():
     return psycopg2.connect(os.environ["DATABASE_URL"])
 
-def get_connection():
-    return psycopg2.connect(
-        dbname="portfolio_db",
-        user="postgres",
-        password="Vai@post",
-        host="localhost"
-    )
-
